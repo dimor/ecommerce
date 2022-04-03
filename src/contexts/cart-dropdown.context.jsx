464d4773ea13a74f409fe68dac3383495404cc68
Dima Max <dimorm@gmail.com>
2022-04-03 20:48:10 +0300

    added cart item

--- src/contexts/cart-dropdown.context.jsx
+++ src/contexts/cart-dropdown.context.jsx
@@ -1,8 +1,15 @@
 import {createContext, useState } from "react";
 
+const addCartItem =(cartItems,productToAdd)=>{
+
+}
+
+
 export const CartDropdownContext = createContext({
     isDropdownOpen:false,
-    setDropdown:()=>{}
+    setDropdown:()=>{},
+    cartItems:[],
+    addItemToCart:()=>{}
 });
 
 
@@ -12,6 +19,12 @@ export const CardDropdownProvider =({children})=>{
     const [isDropdownOpen,setDropdown] = useState(false);
 
     const value ={isDropdownOpen,setDropdown}
+
+    const [cartItems,setCartItems] = useState([]);
+
+    const addItemToCart =(productToAdd)=>{
+        setCartItems(addCartItem(cartItems,productToAdd))
+    }
 
     return <CartDropdownContext.Provider value={value}>{children}</CartDropdownContext.Provider>
 }
