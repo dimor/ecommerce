9802c85493c72a3d8c0edbd7557dcf42d6fd1399
Dima Max <dimorm@gmail.com>
2022-04-10 06:54:19 +0300

    reducers

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+
+
+const MenuItem = ({ title, imageUrl, size, linkUrl}) => {
+
+  const navigate = useNavigate();
+  
+  const navigateHandler = ()=> navigate(linkUrl);
+
+  return(
   <div className={`${size} menu-item`}>
     <div
       style={{
@@ -9,11 +18,11 @@ const MenuItem = ({ title, imageUrl, size }) => (
       }}
       className="background-image"
     ></div>
-    <div className="content">
+    <div onClick={navigateHandler} className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
-);
+)};
 
 export default MenuItem;
